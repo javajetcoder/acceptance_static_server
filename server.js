928378ccebf0591e65ccdf3e85553693b67cde77
Jeffrey Griffin <jeff@javajet.com>
2023-11-10 11:16:43 -0500

    keycloak protection

--- server.js
+++ server.js
@@ -4,9 +4,25 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+/*keycloak start*/
+const Keycloak = require('keycloak-connect');
+const session = require('express-session');
+//Set up keycloak
+var memoryStore = new session.MemoryStore();
+var keycloak = new Keycloak({ store: memoryStore });
+//keycloak session                       
+app.use(session({
+    secret: 'BeALongSecret',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+}));
+app.use(keycloak.middleware());
+/*keycloak end*/
+
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res) {
+app.get('/', keycloak.protect(), function(req, res) {
   res.redirect('static/index.html');
 });
 
